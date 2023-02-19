0fe7e883e0d8b64f8862e1f6dfd52bd743e825bb
Varma Aniket <vaan.origin@gmail.com>
2023-02-19 23:27:07 +0530

    OTP Verification Test 1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,51 @@ require('dotenv').config();
 
 const bcrypt = require('bcrypt');
 
+
+// NodeMailer
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function NodeMailer(receiverEmail, otp) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    requireTLS: true,
+    auth: {
+      user: "varmajay109@gmail.com", // generated ethereal user
+      pass: "ogsqlmrcsmsixgbf", // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"FaceRecognitionAttendenceApp"<varmajay109@gmail.com>', // sender address
+    to: `${receiverEmail}`, // list of receivers
+    subject: "Verification OTP", // Subject line
+    text: "", // plain text body
+    html: `This is your Verification Code <center><b><h1>${otp}</h1></b></center>`, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+
+
+
+//
+
 router.post('/signup', (req, res) => {
   console.log('sent by client ', req.body);
   const { name, email, password, dob } = req.body;
@@ -38,6 +83,35 @@ router.post('/signup', (req, res) => {
     })
 });
 
+router.post('/verify', (req, res) => {
+  console.log('sent by client ', req.body); 
+  const { name, email, password, dob } = req.body;
+  if (!name || !email || !password || !dob) {
+    return res.status(422).send({ error: 'Please add all the fields' });
+  }
+  User.findOne({ email: email })
+    .then(async (savedUser) => {
+      if (savedUser) {
+        return res.status(422).send({ error: "Already Existed User" });
+      }
+      try {
+        let otp = Math.floor(1000 + Math.random() * 9000);
+        let user ={
+          name,
+          email,
+          password,
+          dob,
+          otp,
+        }
+        await NodeMailer(email, otp).catch(console.error);
+        res.send({ message: "OTP sent to your email",userData:user });
+      } catch (err) {
+        console.log('OTP error', err)
+        return res.status(422).send({ error: err.message });
+      }
+    })
+})
+
 router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -50,17 +124,17 @@ router.post('/signin', async (req, res) => {
   }
 
   try {
-     bcrypt.compare(password, savedUser.password, (err, result) =>{
-       if(result){
+    bcrypt.compare(password, savedUser.password, (err, result) => {
+      if (result) {
         // console.log('password matched');
-         const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET);
-         res.send({ token });
-       } 
-      else{
+        const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET);
+        res.send({ token });
+      }
+      else {
         //  console.log('password not matched')
         return res.status(422).send({ error: 'password not matched' });
       }
-    
+
     });
 
   }
